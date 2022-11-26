51f39d7d6271c78d23e18c0252eaee6b0fbd42fd
Vivian Dai <38384400+vivian-dai@users.noreply.github.com>
2022-11-26 11:41:48 -0500

    attempts to do things

--- weather_app/src/App.js
+++ weather_app/src/App.js
@@ -1,6 +1,22 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [city, setCity] = useState("");
+
+  const search = () => {
+    fetch("https://geocode.maps.co/search?q=asdguaysgd").then((response) => {
+      console.log(response);
+      setCity(searchQuery);
+    })
+  }
+
+  const handleTyping = (event) => {
+    setSearchQuery(event.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -8,7 +24,13 @@ function App() {
         <h1>Weather Thing App</h1>
       </header>
       <main>
-
+        Search for your city: <input
+         type="text"
+         value={searchQuery}
+         onChange={handleTyping}
+         ></input>
+        <button onClick={search}>Search</button>
+        <div>{}</div>
       </main>
       <footer className="footer">
         Page created by Cynthia Lei
